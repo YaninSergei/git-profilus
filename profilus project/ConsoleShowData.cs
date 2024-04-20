ad9cda50336528ba0cbb13bd0e7a84916f4f46ea
Yanin <syanin@inbox.ru>
2024-04-20 17:30:43 +0700

    Рефакторинг кода алгоритма обработки данных

---
+++ profilus project/ConsoleShowData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace profilus_project
+{
+    class ConsoleShowData : IShowEntries
+    {
+        public void show_entries(List<Entry> prientries)
+        {
+            Console.WriteLine(prientries);
+            Console.WriteLine(prientries.Count);
+            for (int i = 0; i < prientries.Count; i++)
+            {
+                Console.Write("Ключ:" + prientries[i].key);
+                Console.WriteLine("Значение:" + prientries[i].value);
+
+            }
+        }
+    }
+}
---
+++ profilus project/IShowEntries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static profilus_project.Entry;
+using static profilus_project.ConsoleShowData;
+
+namespace profilus_project
+{
+    interface IShowEntries
+    {
+
+        void show_entries(List<Entry> prientries);
+    }
+
+}
